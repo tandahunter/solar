a7a1abd4953d0d4e9c5600b338107aa40577a5d9
tandahunter <adam_jones_1985@hotmail.com>
2017-07-16 12:33:14 +0100

    Planets now orbit successfully

--- solar.go
+++ solar.go
@@ -9,10 +9,14 @@ import (
 //G Gravitational constant
 const G = 6.67384e-11
 
-//SEC_PER_STEP is the number of seconds in the sim per step
-const SEC_PER_STEP = 8
-const STEPS_PER_FRAME = 10000
-const METERS_PER_UNIT = 1000000000
+//SecondsPerStep is the number of seconds in the sim per step
+const SecondsPerStep = 8
+
+//StepsPerFrame determines how many calculations should be calculated per cycle
+const StepsPerFrame = 10000
+
+//MetersPerUnit is a gigameter
+const MetersPerUnit = 1000000000
 
 func getAcceleration(distance, starMass float64) float64 {
 	return G * starMass / (math.Pow(distance, float64(2)))
@@ -20,34 +24,37 @@ func getAcceleration(distance, starMass float64) float64 {
 
 func main() {
 	solarBodies := *solarutil.NewPlanetArray()
-	solarBodies = append(solarBodies, *solarutil.NewStar("Sun", 50000, 0, 0, 0))
-	solarBodies = append(solarBodies, *solarutil.NewPlanet("Mercury", 500, 600, 0, 0))
-	solarBodies = append(solarBodies, *solarutil.NewPlanet("Venus", 600, 800, 0, 0))
-	solarBodies = append(solarBodies, *solarutil.NewPlanet("Earth", 1000, 1000, 0, 0))
-	solarBodies = append(solarBodies, *solarutil.NewPlanet("Mars", 800, 2000, 0, 0))
-
+	solarBodies = append(solarBodies, *solarutil.NewStar("Sun", 1.988435e30, 0, 0, 0))
+	solarBodies = append(solarBodies, *solarutil.NewPlanet("Earth", 5.9721986e24, 150, 0, 0, solarutil.NewVector(0, 0, 2.963e-5)))
 	star := solarBodies.GetStars().FirstOrDefault()
 
 	planets := solarBodies.GetPlanets()
 	planets.PrintNames()
 
-	for _, i := range *planets {
-		performOrbitalManoeuvre(star, &i)
+	for x := 0; x < 2000; x++ {
+		for _, i := range *planets {
+			performOrbitalManoeuvre(star, &i)
+		}
 	}
+
+	earth := solarBodies.GetPlanetsByName("Earth").FirstOrDefault()
+	earth.PrintVector()
 }
 
 func performOrbitalManoeuvre(star, planet *solarutil.Planet) {
 	vel := solarutil.NewVector(0, 0, 0)
 	speed := float64(0)
 
-	for i := 0; i < STEPS_PER_FRAME; i++ {
-		speed = getAcceleration(star.DistanceTo(planet), star.Mass)
+	for i := 0; i < StepsPerFrame; i++ {
+		distance := star.DistanceTo(planet)
+		speed = getAcceleration(distance*MetersPerUnit, star.Mass) * SecondsPerStep
+
 		vel.SubVectors(star.Vector, planet.Vector)
-		vel.SetLength(speed / METERS_PER_UNIT)
+		vel.SetLength(speed / MetersPerUnit)
 		planet.Velocity.Add(vel)
 
-		planet.Vector.X += planet.Velocity.X * SEC_PER_STEP
-		planet.Vector.Y += planet.Velocity.Y * SEC_PER_STEP
-		planet.Vector.Z += planet.Velocity.Z * SEC_PER_STEP
+		planet.Vector.X += planet.Velocity.X * SecondsPerStep
+		planet.Vector.Y += planet.Velocity.Y * SecondsPerStep
+		planet.Vector.Z += planet.Velocity.Z * SecondsPerStep
 	}
 }
