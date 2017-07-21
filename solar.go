127f619b83bde51873cd4626fa70b9734589ecb4
Adam Jones <AJONES@FARGOEXE.LOCAL>
2017-07-21 13:33:30 +0100

    Didn't commit the go

--- solar.go
+++ solar.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math"
 	"net/http"
@@ -11,6 +13,7 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 	"github.com/tandahunter/solarutil"
 )
 
@@ -23,16 +26,28 @@ const (
 	StepsPerFrame = 10000
 	//MetersPerUnit is a gigameter
 	MetersPerUnit = 1000000000
+	//WebServerPort is the port on which to open the web router
+	WebServerPort = 8080
+	//WebSocketPort is the port on which to open the web socket
+	WebSocketPort = 8081
 )
 
 var planets *solarutil.PlanetArray
 var sun *solarutil.Star
+var manoeuvreCount = 0
 
 func main() {
+	//Create some planets, and a sun
 	initSun()
 	initPlanets()
+
+	//Initiate a ticker to start generating some data / orbiting the planets
 	initFrameTicker()
 
+	//Determine how to make the data available.
+	//web = simple get requests only
+	//websocket = get requests for the initial data, and a websocket
+	//for the live calculations
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "web":
@@ -43,13 +58,21 @@ func main() {
 }
 
 func initRouter() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/Sun/", getSun)
-	router.HandleFunc("/Planets/", getPlanets)
-	router.HandleFunc("/Planets/{id}/", getPlanet)
-	router.HandleFunc("/Planets/{id}/Vector/", getPlanetVector)
+	go func() {
+		router := mux.NewRouter().StrictSlash(true)
+		router.HandleFunc("/Sun/", getSun)
+		router.HandleFunc("/Client/", getClient)
+		router.HandleFunc("/Planets/", getPlanets)
+		router.HandleFunc("/Planets/{id}/", getPlanet)
+		router.HandleFunc("/Planets/{id}/Vector/", getPlanetVector)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", WebServerPort), router))
+	}()
+
+	fmt.Printf("Solar is listening on port:%d", WebServerPort)
+
+	http.HandleFunc("/Planets/", streamPlanets)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", WebSocketPort), nil))
 }
 
 func initSun() {
@@ -62,7 +85,7 @@ func initPlanets() {
 	planets.AddNewPlanet(2, "Venus", 4.86732e24, 108.8, 3.5e-5)
 	planets.AddNewPlanet(3, "Earth", 5.9721986e24, 150, 2.963e-5)
 	planets.AddNewPlanet(4, "Mars", 6.41693e23, 227.94, 0.0000228175)
-	//planets.AddNewPlanet(5, "Jupiter", 1.89813e27, 778.33, 0.0000129824)
+	planets.AddNewPlanet(5, "Jupiter", 1.89813e27, 778.33, 0.0000129824)
 }
 
 func initFrameTicker() {
@@ -73,8 +96,25 @@ func initFrameTicker() {
 			for _, planet := range *planets {
 				performOrbitalManoeuvre(sun, planet)
 			}
+			manoeuvreCount++
 		}
 	}()
+}
+
+func getClient(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	} else {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		f, err := ioutil.ReadFile("resources/client.html")
+		if err != nil {
+			http.Error(w, "Failed dependency", http.StatusFailedDependency)
+		} else {
+			w.Write(f)
+		}
+	}
 }
 
 func getSun(w http.ResponseWriter, r *http.Request) {
@@ -143,6 +183,26 @@ func getPlanet(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func streamPlanets(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
+	c, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println("Could not open websocket connection")
+		return
+	}
+	defer c.Close()
+
+	for {
+		if err = c.WriteJSON(planets); err != nil {
+			log.Println(err)
+			break
+		}
+
+		time.Sleep(1000)
+	}
+}
+
 func performOrbitalManoeuvre(star *solarutil.Star, planet *solarutil.Planet) {
 	vel := solarutil.NewVector(0, 0, 0)
 	speed := float64(0)
@@ -159,8 +219,6 @@ func performOrbitalManoeuvre(star *solarutil.Star, planet *solarutil.Planet) {
 		planet.Vector.Y += planet.Velocity.Y * SecondsPerStep
 		planet.Vector.Z += planet.Velocity.Z * SecondsPerStep
 	}
-
-	planet.ManouevreCount++
 }
 
 func getAcceleration(distance, starMass float64) float64 {
